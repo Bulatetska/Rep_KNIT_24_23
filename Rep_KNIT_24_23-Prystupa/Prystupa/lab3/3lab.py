c1b5d95fb439ae14b92e5eb735963df125165f68
MrDimaa <126572355+MrDimaa@users.noreply.github.com>
2023-03-07 19:12:09 +0200

    Create 3lab.py
    lab3

---
+++ Rep_KNIT_24_23-Prystupa/Prystupa/lab3/3lab.py
@@ -0,0 +1,9 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lst = [x for x in lst if x % 2 != 0]
+print(lst)
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lst = [x**2 for x in lst]
+print(lst)
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+max_num = max(lst)
+print(max_num)
