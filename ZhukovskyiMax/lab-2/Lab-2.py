f740ad7de132be01b5b89ff425096068c52fca2c
Archerusoff <>
2023-03-13 09:12:12 +0200

    Lab-2

---
+++ ZhukovskyiMax/lab-2/Lab-2.py
@@ -0,0 +1,23 @@
+price = float(input("Enter price: "))
+
+
+
+
+
+
+def serchSell(price):
+    if price > 1000:
+        pricediscount = 0.1
+    elif price > 500:
+        pricediscount = 0.05
+    elif price > 100:
+        pricediscount = 0.03
+    else:
+        pricediscount = 0
+
+    sum = price * pricediscount
+    allPrice = price - sum
+    print(f"discount: {allPrice:.2f} $")
+
+
+serchSell(price)
