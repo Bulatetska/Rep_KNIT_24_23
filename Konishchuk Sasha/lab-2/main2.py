e7403281caef6582cb8e6336a260c7907db06990
alex Ko <konishchuk.sasha@gmail.com>
2023-03-17 09:11:09 +0200

    lab-2(2)

--- Konishchuk Sasha/lab-2/main2.py
+++ Konishchuk Sasha/lab-2/main2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # 2. Використання тернарного оператора для перевірки порожнього рядка
-text = raw_input ("Input text:")
-result = text if text != "" else 'None'
+text = input ("Input text:")
+result = text if text else 'None'
 print(result)
