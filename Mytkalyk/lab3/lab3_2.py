43973fd8d74817e0639a7722740852205a7540c7
maksmt <maksmtoff@gmail.com>
2023-04-07 11:39:58 +0300

    Lab3

---
+++ Mytkalyk/lab3/lab3_2.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+new_list = []
+for x in my_list:
+    new_list.append(x**2)
+
+print(new_list)  # [1, 4, 9, 16, 25, 36, 49, 64, 81]
---
+++ Mytkalyk/lab3/lab3_3.py
@@ -0,0 +1,5 @@
+my_list = [1, 5, 2, 8, 3, 54, 23, 213, 1211, 23]
+
+max_element = max(my_list)
+
+print(max_element)
