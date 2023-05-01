cd3f48f64f381a02e03aa5d59c18376d3baf7138
nadiykaa <nadiyka.vitkovets@gmail.com>
2023-05-01 18:44:25 +0300

    lab8

---
+++ vitkovets/lab8/main (2).py
@@ -0,0 +1,13 @@
+
+from example_module import main
+
+a = 2
+b = -5
+c = 6
+
+x1, x2 = main(a, b, c)
+
+if x1 is not None:
+    print(f"Рівняння має корені: x1 = {x1}, x2 = {x2}")
+else:
+    print("Рівняння не має дійсних коренів")
