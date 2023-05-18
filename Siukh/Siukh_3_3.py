5fb077bfe25da3531eb0fad918b1415243875564
SSiukh <126235275+SSiukh@users.noreply.github.com>
2023-05-19 00:42:03 +0300

    лабораторна 3

---
+++ Siukh/Siukh_3_3.py
@@ -0,0 +1,6 @@
+amount = int(input("Задайте довжину списку:"))
+array = input("Введіть елементи списку:")
+numbers = list(map(int, array.split(' ')))
+print(numbers)
+maxElement = max(numbers)
+print(maxElement)
