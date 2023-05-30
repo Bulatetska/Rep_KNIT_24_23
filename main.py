0876b34754bc4346a2db752dc5214ca704274a66
Max-Zhukovskyi <126571996+Max-Zhukovskyi@users.noreply.github.com>
2023-05-30 12:23:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import heiko
 import mishchuk
 import test
-import Zhukovskyi
 
 print(mishchuk.add(1, 2))
 test.printTest()
 
 print("Reversed text:", heiko.reverse_string("Hello, World!"))
 print("Capitalized text:", heiko.capitalize_string("Hello, World!"))
-
-# Приклад використання:
-array = [9, 5, 2, 7, 1]
-Zhukovskyi.sort_and_divide(array)
